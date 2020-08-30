2795ebccbef8c16eba4639689fded3ae6a0a65ed
David Tkeshelashvili <dtkeshelashvili07@gmail.com>
2020-08-30 18:27:08 +0400

    add adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -15,3 +15,21 @@ testWebP(function (support) {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.nav__menu'),
+		menuItem = document.querySelectorAll('.menu__item'),
+		hamburger = document.querySelector('.hamburger');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger__active');
+		menu.classList.toggle('menu__active');
+	});
+
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger__active');
+			menu.classList.toggle('menu__active');
+		})
+	})
+})
